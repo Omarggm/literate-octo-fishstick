9a4e289e1b66f505cd729bc444aae8bb3e8d3198
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-13 09:36:30 -0700

    Get route for api/users

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,8 +7,19 @@ const createUser = async (req, res) => {
     const newUser = await User.create({ username, email });
     res.json(newUser);
   } catch (error) {
+    console.log(error);
     res.status(500).json(error);
   }
 };
 
-module.exports = { createUser };
+const getAllUsers = async (req, res) => {
+try {
+  const users = await User.find({});
+  res.json(users);
+} catch (error) {
+  console.log(error);
+  res.status(500).json(error);
+}
+};
+
+module.exports = { createUser , getAllUsers };
--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,6 +1,8 @@
-const { createUser } = require('../../../controllers/userController');
+const { createUser, getAllUsers } = require('../../../controllers/userController');
 const router = require('express').Router();
 
 router.post('/', createUser);
+
+router.get('/', getAllUsers)
 
 module.exports = router;
